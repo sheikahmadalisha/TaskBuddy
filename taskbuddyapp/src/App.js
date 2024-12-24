fb7939fa88ac2699ba3d962de435c0f4870f8944
sheikahmadalisha <skahmadalisha31@gmail.com>
2024-12-24 16:03:04 +0530

    final code

--- taskbuddyapp/src/App.js
+++ taskbuddyapp/src/App.js
@@ -1,8 +1,59 @@
+import { useState , useEffect} from "react";
 import TaskForm from "./components/TaskForm";
+import ProgressTracker from "./components/ProgressTracker";
+import './Style.css'
+import TaskList from "./components/TaskList";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+  const addTask = (task)=> {
+    setTasks([...tasks, task])
+  };
+
+  const updateTask = (index, updatedTask) => {
+    const newTasks = [...tasks];
+    newTasks[index] = updateTask;
+    setTasks(newTasks);
+  };
+
+  const deleteTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  const clearTasks = () => {
+    setTasks([]);
+  };
+
   return (
-    <TaskForm/>
+    <div className="App">
+      <header class="header">
+        <div class="header-container">
+        <h1 class="title">TaskBuddy</h1>
+        <p class="taagline"> Your friendly task manager</p>
+        </div>
+
+      </header>
+
+    <TaskForm addTask = {addTask} />
+    <TaskList
+      tasks={tasks}
+      updateTask={updateTask}
+      deleteTask={deleteTask}/>
+
+      <ProgressTracker tasks={tasks}/>
+
+      {tasks.length > 0 && (
+        <button className="clear-btn" onClick={clearTasks}>
+        Clear All Tasks
+      </button>
+      )}
+      
+    </div>
+    
   );
 }
 
--- taskbuddyapp/src/components/TaskForm.jsx
+++ taskbuddyapp/src/components/TaskForm.jsx
@@ -1,14 +1,45 @@
 import React, { useState } from "react";
 
-export default function TaskForm() {
+export default function TaskForm({addTask}) {
     const [task, setTask] = useState('');
+    const [priority, setPriority] = useState('');
+    const [category, setCategory] = useState('');
+
+    const handleSubmit =(e)=> {
+        
+         addTask({text: task, priority, category, completed: false});
+         setPriority('Medium');
+         setCategory('General');
+         setTask("");
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit} className="task-form">
+            <div id="inp">
             <input type="text"
             placeholder="Enter Your Task"
             required
             value={task}
             onChange={(e)=> setTask(e.target.value)}/>
+            <span><button>Add Tasks</button></span>
+
+            </div>
+            <div className="btns">
+                <select value={priority} onChange={(e)=> setPriority(e.target.value)}>
+                    
+                    <option>Medium</option>
+                    <option>Low</option>
+                    <option>High</option>
+
+                </select>
+
+                <select value={category} onChange={(e)=> setCategory(e.target.value)}>
+                    <option>General</option>
+                    <option>Work</option>
+                    <option>Personal</option>
+
+                </select>
+            </div>
 
             <h1>{task}</h1>
         </form>
